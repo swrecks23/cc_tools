94fe2926d2dd269f8ca7e2f033c89dc26457f7a4
Seth Henry <swrecks@gmail.com>
2019-02-26 14:10:59 -0500

    Changes to Chip's Challenge

--- part_3_convert_json.py
+++ part_3_convert_json.py
@@ -1,40 +1,58 @@
 import cc_dat_utils
 import json
+import cc_data
 
 
-def make_level_library_from_json(json_data):
+def make_cc_level_from_json(level_data):
 
-	level_library = cc_data.CCDataFile()
-	for level_data in json_data["Levels"]:
-		level = test_data.CCLevel()
-		level_library.time = level_data["time"]
-		level_library.level_number = level_data["level_number"]
-		level_library.num_chips = level_data["num_chips"]
-		level_library.optional_fields = level_data["optiona_fields"]
-		level_library.upper_layer = level_data["upper_layer"]
+	# level_library = cc_data.CCDataFile()
+	new_level = cc_data.CCLevel()
+	new_level.level_number = level_data["level_number"]
+	new_level.num_chips = level_data["num_chips"]
+	new_level.upper_layer = level_data["upper_layer"]
+	new_level.time = level_data["time"]
+	for field_json in level_data["optional_fields"]:
+		new_field = None
 		
-
-
-
-
-	pfgd_test_data = cc_dat_utils.make_cc_data_from_dat("data/sehenry_cc1.json")
-	cc_dat_utils.write_cc_data_to_dat(pfgd_test_data, "data/copy_of_sehenry_cc1.json")
-	pfgd_test_data
+		
+		print("field_json: "+str(field_json))
+		if field_json["id"] == "Hint":
+			new_field = cc_data.CCMapHintField(field_json["hint_data"])
+			new_level.add_field(new_field)
+		if field_json["id"] == "Password":
+			new_field = cc_data.CCEncodedPasswordField(field_json["Password_data"])
+			new_level.add_field(new_field) 
+		if field_json["id"] == "title":
+			new_field = cc_data.CCMapTitleField(field_json["title_data"])
+			new_level.add_field(new_field)
+		elif field_json["id"] == "monsters":
+			monster_coords = []
+			for monster_json in field_json["monster_data"]:
+				monster_coords.append(cc_data.CCCoordinate(monster_json[0],monster_json[1]))
+			new_field = cc_data.CCMonsterMovementField(monster_coords)
+			new_level.add_field(new_field)
+	return new_level
 
 #Part 3
 #Load your custom JSON file
 with open ("data/sehenry_cc1.json","r") as reader:
 	test_json = json.load(reader)
 #Convert JSON data to cc_data
-cc_data = make_level_library_from_json(test_json)
+new_level_library = cc_data.CCDataFile()
+for level_json in test_json["Levels"]:
+	new_level = make_cc_level_from_json(level_json)
+	new_level_library.add_level(new_level)
+print(new_level_library)
+
+# cc_level_data = make_cc_level_from_json(test_json)
+
 #Save converted data to DAT file
+cc_dat_utils.write_cc_data_to_dat(new_level_library, "data/sehenry_cc1.dat")
 
-print(pfgd_test_data)
-
-
-
-
+# # print(pfgd_test_data)
+# print(cc_level_data)
 
 
 
-print(make_level_library_from_json(test_json))
+
+
